3fa5c6d15746f9e89111ef4dafae9ab9193a71ee
Smith-Dabre <dabresmith97@gmail.com>
2025-07-23 15:49:26 -0400

    Initial commit for backend

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+const authRoutes = require('./routes/authRoutes');
+const productRoutes = require('./routes/productRoutes');
+const cartRoutes = require('./routes/cartRoutes');
+const paymentRoutes = require('./routes/paymentRoutes');
+
+dotenv.config();
+
+connectDB(); // Connect to MongoDB
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// API Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/cart', cartRoutes);
+app.use('/api/payment', paymentRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
